002b512b9452d990481834bcda9c54db53b128a6
Giri <121802870+Girii4@users.noreply.github.com>
2024-08-27 17:30:24 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,7 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
-import logo from "../../assets/logo.png";
+import logo from "../../assets/logoo.png";
 
 const Headers = styled.header`
   display: flex;
